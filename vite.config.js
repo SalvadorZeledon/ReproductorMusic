21824a4105d4fdbf1fe7797818d5ac624dd40587
SalvadorZeledon <salvadorzeledon9@gmail.com>
2025-12-28 14:34:04 -0600

    agregando cambios

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// Si lo vas a publicar en GitHub Pages dentro de un repo,
-// cambia base a '/NOMBRE_DEL_REPO/'.
 export default defineConfig({
   plugins: [react()],
-})
+  base: "/ReproductorMusic/",
+});
